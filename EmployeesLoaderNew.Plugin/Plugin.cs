0deb6bb3764fc216308002ff15786f593517e719
lasosi01 <lasosi01@mail.ru>
2025-06-25 10:05:57 +0500

    Plagin: Измеенение перехвата исключений и логики возврата сотрудников
    Вместо переброса исключений на уровень выше, теперь они обрабатываются в блоке catch, чтобы не останавливать работу приложения, так же из за этого в методе GetUsersFromApi и GetJArrayб, теперь в любом случае будет возвращаться данные вне зависимости пустые они или нет, убран лишний перехвать исключения в методе Run, в силу ненадобности, вместо возврата нового списка сотрудников, новый список добавляется к старому, в силу специфики задания

--- EmployeesLoaderNew.Plugin/Plugin.cs
+++ EmployeesLoaderNew.Plugin/Plugin.cs
@@ -23,41 +23,38 @@ namespace LoaderNewEmployees
         #region Основной метод запуска
 
         /// <summary>
-        /// Запускает процесс загрузки сотрудников.
+        /// Загружает сотрудников из внешнего API и добавляет их к существующим данным.
+        /// Если загрузка не удалась, возвращает исходные данные без изменений.
         /// </summary>
-        /// <param name="args">Входные данные (не используются).</param>
-        /// <returns>Коллекция загруженных сотрудников в виде DataTransferObject.</returns>
+        /// <param name="args">Исходная коллекция сотрудников.</param>
+        /// <returns>Объединённая коллекция сотрудников.</returns>
         public IEnumerable<DataTransferObject> Run(IEnumerable<DataTransferObject> args)
         {
-            try
-            {
-                logger.Info("Loading employees");
+            logger.Info("Loading employees");
 
-                var employeesList = GetUsersFromApi(URL_ADRESS);
+            var employeesList = GetUsersFromApi(URL_ADRESS);
 
-                logger.Info($"Loaded {employeesList.Count} employees");
+            logger.Info($"Loaded {employeesList.Count} employees");
 
-                return employeesList.Cast<DataTransferObject>();
-            }
+            var newArgs = args.Concat(employeesList.Cast<DataTransferObject>());
 
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Ошибка при загрузке сотрудников");
-                throw;
-            }
+            return newArgs;
         }
         #endregion
 
         #region Вспомогательные методы
 
         /// <summary>
-        /// Получает список сотрудников из API по указанному URL.
+        /// Загружает сотрудников из внешнего API и добавляет их к существующим данным.
+        /// Если загрузка не удалась, возвращает исходные данные без изменений.
         /// </summary>
-        /// <param name="url">URL API для загрузки пользователей.</param>
-        /// <returns>Список сотрудников в виде объектов EmployeesDTO.</returns>
+        /// <param name="args">Исходная коллекция сотрудников.</param>
+        /// <returns>Объединённая коллекция сотрудников.</returns>
         private List<EmployeesDTO> GetUsersFromApi(string url)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var users = new List<EmployeesDTO>();
 
             using (var client = new WebClient())
             {
@@ -65,7 +62,7 @@ namespace LoaderNewEmployees
                 {
                     var jArray = GetJArray(url, client);
 
-                    var users = jArray
+                    users = jArray
                         .Select(item =>
                         {
                             var firstName = (string)item["firstName"] ?? string.Empty;
@@ -81,44 +78,45 @@ namespace LoaderNewEmployees
                             return user;
                         })
                         .ToList();
-
-                    return users;
                 }
-
                 catch (Exception ex)
                 {
                     logger.Error(ex, "Ошибка при обработке списка пользователей из JSON.");
-                    return new List<EmployeesDTO>();
                 }
+
+                return users;
             }
         }
 
         /// <summary>
         /// Загружает JSON с указанного URL и возвращает массив пользователей.
+        /// Если массив 'users' отсутствует или JSON некорректен, возвращает пустой массив и логирует предупреждение.
         /// </summary>
         /// <param name="url">URL для загрузки JSON.</param>
-        /// <param name="client">Экземпляр WebClient для загрузки данных.</param>
-        /// <returns>JArray с пользователями.</returns>
-        /// <exception cref="Exception">Если JSON не содержит массив 'users' или произошла ошибка загрузки/парсинга.</exception>
+        /// <param name="client">WebClient для загрузки данных.</param>
+        /// <returns>Массив пользователей или пустой массив при ошибках.</returns>
         private JArray GetJArray(string url, WebClient client)
         {
+            var jArray = new JArray();
+
             try
             {
                 var json = client.DownloadString(url);
 
                 var jObject = JObject.Parse(json);
-                var jArray = jObject["users"] as JArray;
+                jArray = jObject["users"] as JArray;
 
                 if (jArray == null)
-                    throw new Exception("В JSON отсутствует массив 'users' или он имеет неверный формат.");
+                    logger.Warn("В JSON отсутствует массив 'users' или он имеет неверный формат.");
 
                 return jArray;
             }
             catch (Exception ex)
             {
                 logger.Error(ex, "Ошибка при получении массива 'users' из JSON");
-                throw new Exception("Ошибка при загрузке или парсинге JSON с URL: " + url, ex);
             }
+
+            return jArray;
         }
         #endregion
     }
